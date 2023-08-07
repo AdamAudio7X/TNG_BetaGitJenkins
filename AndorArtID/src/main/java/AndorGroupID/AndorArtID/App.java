d6bb7137aa02cd41345a74ded18697b72a56bc03
AdamAudio7X <peterklimqa@gmail.com>
2023-08-07 14:27:51 -0700

    First Commit - Added Project

---
+++ AndorArtID/src/main/java/AndorGroupID/AndorArtID/App.java
@@ -0,0 +1,13 @@
+package AndorGroupID.AndorArtID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
